02f8c62ca1b701da602681d316e0e7fbf360fd9c
Joshua Escribano-Fontanet <joshua@Joshuas-MacBook-Pro.local>
2013-08-27 14:43:52 -0400

    Minor Route Change.

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Buffalohostage::Application.routes.draw do
   end
   resources :sessions, only: [:new, :create, :destroy]
   #resources :songs, only: [:index]
+  get '/bands',   to: 'users#index'
   get '/signup',  to: 'users#new'
   get '/signin',  to: 'sessions#new'
   get '/signout', to: 'sessions#destroy'
