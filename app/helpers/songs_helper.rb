8f79ef93e835834bda16c6ebbd830520d4b3e496
Joshua Escribano-Fontanet <joshua@Joshuas-MacBook-Pro.local>
2013-08-31 01:33:26 -0400

    Added Tables.

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,12 +1,2 @@
 module SongsHelper
-
-  # Format the rendering of the song titles
-  def format_song(song, user)
-    if is_band?(user)
-      "Title: " + song.title + " is private?: " + song.privacy.to_s.titleize
-    else
-      "Title: " + song.title
-    end
-  end
-
 end
