ab6de6ed0b5b0e39b5786d0c535431abfe81e09c
Joshua Escribano-Fontanet <joshua@Joshuas-MacBook-Pro.local>
2013-09-03 00:06:34 -0400

    Minor Bug.

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -3,7 +3,7 @@ module SongsHelper
   def url_for_song(user, song)
     url = "https://s3.amazonaws.com/buffalo_hostage/"
     url = url + user.name.gsub(/\s/, '+') + '/'
-    url = url + song.title + '.mp3'
+    url = url + song.title.gsub(/\s/, '_') + '.mp3'
   end
 
 end
